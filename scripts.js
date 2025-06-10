032859985fe6374fbb8464f0da322cdff00e768a
jsncrz <jasoon.acruz@gmail.com>
2025-06-10 15:01:01 -0700

    Add slideshow for images
    This commit also adds improvement to the landing screen

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
-const title = ["Hi, I'm Jason.","A passionate Full Stack Developer.", "Let's build something powerful together."];
-writeChars('hero-title', title, 0, 0);
+const title = ["Hi, I'm Jason.", "A passionate Full Stack Developer.", "Let's build something powerful together."];
+writeChars('hero-text', title, 0, 0);
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -10,6 +10,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+let slideIndex = 1;
+setInterval(showSlides, 5000);
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slide");
+    if(slideIndex == slides.length + 1) {slideIndex = 1};
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    slideIndex++;
+}
 function writeChars(elementId, text, index, lineIndex) {
     if (lineIndex < text.length) {
         if (index < text[lineIndex].length) {
@@ -17,10 +29,18 @@ function writeChars(elementId, text, index, lineIndex) {
             index++;
             setTimeout(writeChars, 50, elementId, text, index, lineIndex);
         } else {
-            document.getElementById('hero-title').appendChild(document.createElement("br"));
+            document.getElementById('hero-text').appendChild(document.createElement("br"));
             lineIndex++;
             index = 0;
             writeChars(elementId, text, index, lineIndex);
         }
+    } else {
+        document.getElementById('hero-button').classList.remove('hidden');
     }
+}
+function goToSection(sectionId) {
+    console.log(sectionId)
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: 'smooth'
+    });
 }
